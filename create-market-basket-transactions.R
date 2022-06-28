1d7f12916524e60e3861aa159b54de28b836bb81
Michael <maew711@gmail.com>
2022-06-28 10:19:18 -0400

    Add wrapper function for creating market basket CSV file

--- create-market-basket-transactions.R
+++ create-market-basket-transactions.R
@@ -2,11 +2,13 @@
 
 #library(stringr)
 
-create_market_basket <- function(colname) {
+create_market_basket <- function(source.df, colname, filename) {
   # Description: Create market basket transactions from ACS data frame. 
   #              Write output csv file
   # 
-  # args: colname - The name of the column in ACS data frame
+  # args: source.df:  source data frame 
+  #       colname:    The name of the column in ACS data frame
+  #       filename:   The name of output file to create
   #
 
   # split column name on underscore character
@@ -18,13 +20,13 @@ create_market_basket <- function(colname) {
   output.df <- data.frame(matrix(ncol = number.cols, nrow = 0))
   row.number <- nrow(output.df) + 1
   
-  # iterate through acs.df data frame
-  for (i in 1:nrow(acs.df))
+  # iterate through source.df data frame
+  for (i in 1:nrow(source.df))
   {
-    county.name <- acs.df[i, "County"]
+    county.name <- source.df[i, "County"]
     
-    acs.df.value <- acs.df[i, colname]
-    row.count <- ifelse(is.na(acs.df.value), 0, acs.df.value)
+    source.df.value <- source.df[i, colname]
+    row.count <- ifelse(is.na(source.df.value), 0, source.df.value)
     
     if (row.count != 0) {
       if (length(col.tokens) == 1) {
@@ -44,81 +46,107 @@ create_market_basket <- function(colname) {
       final.df <- output.df[rep(row.number, row.count),]
       
       # write final.df to csv file
-      write.table(final.df, file="data/market_basket_nyc.csv", eol = "\n", quote = FALSE, sep = ",", 
+      write.table(final.df, file=filename, eol = "\n", quote = FALSE, sep = ",", 
                   row.names = FALSE, col.names = FALSE, append = TRUE)
   
     }
   }
 }
 
-# create data frame (for testing)
-acs.df <- read.csv("data/Census_Nyc.csv")
 
-# Create Household.Income_Less50k
-acs.df$Household.Income_Less50k <- acs.df$Household.Income_Less10k + acs.df$Household.Income_10to15k + 
-                                  acs.df$Household.Income_15to25k + acs.df$Household.Income_25to35k +
-                                  acs.df$Household.Income_35to50k
+market_basket_wrapper <- function(input.filename, output.filename) {
+  # Description: Wrapper function for creating new market basket CSV file
+  #
+  # args: input.filename
+  #       output.filename
+  #
+  #
+  
+  # set output file name and remove file from previous run
+  unlink(output.filename)
+  
+  # create data frame
+  acs.df <- read.csv(input.filename)
+  
+  # Create Household.Income_Less50k
+  acs.df$Household.Income_Less50k <- acs.df$Household.Income_Less10k + acs.df$Household.Income_10to15k + 
+                                    acs.df$Household.Income_15to25k + acs.df$Household.Income_25to35k +
+                                    acs.df$Household.Income_35to50k
+  
+  acs.df.subset <- subset(acs.df,
+                          select = -c(Households_WithUnder18,
+                                      Households_WithOver65,
+                                      Over16,
+                                      Over16_Female,
+                                      Over16_Female_Employed,
+                                      Over16_Employed,
+                                      Over16_ArmedForces,
+                                      Under5,
+                                      Age5to9,
+                                      Age10to14,
+                                      Age15to19,
+                                      Age20to24,
+                                      Age25to34,
+                                      Age35to44,
+                                      Age45to54,
+                                      Age55to59,
+                                      Age60to64,
+                                      Over65,
+                                      Under18,
+                                      Over18,
+                                      Over18_Male,
+                                      Over18_Female,
+                                      Over15_Male,
+                                      Over15_Female,
+                                      Over15_Male_Married,
+                                      Over15_Female_Married,
+                                      Nursery.preschool.enrollment,
+                                      Kindergarten.enrollment,
+                                      Householder_Children6to17,
+                                      Total.Population,
+                                      Total.Population.Male,
+                                      Total.Population.Female,
+                                      White,
+                                      Asian,
+                                      Native.American,
+                                      BusinessOccupation,
+                                      SalesOccupation,TransportationOccupation,
+                                      NaturalResourcesOccupation,ServiceOccupation,
+                                      AgricultureIndustry,
+                                      MfgIndustry,
+                                      WholesaleIndustry,
+                                      WarehousingIndustry,
+                                      InformationIndustry, 
+                                      InsuranceIndustry, 
+                                      OtherServicesIndustry,
+                                      EntertainmentIndustry,
+                                      WasteManagementIndustry,
+                                      Household.Income_Less10k, Household.Income_10to15k, 
+                                      Household.Income_15to25k, Household.Income_25to35k,
+                                      Household.Income_35to50k,
+                                      Age19to64_Employed,
+                                      Elementary.school.enrollment,
+                                      Householder_ChildrenUnder6,
+                                      Cohabiting.household))
+  
+  colval.names <- colnames(acs.df.subset)
+  
+  for (c in colval.names[-c(1)]) {
+    create_market_basket(acs.df, c, output.filename)
+  }
 
-acs.df.subset <- subset(acs.df,
-                        select = -c(Households_WithUnder18,
-                                    Households_WithOver65,
-                                    Over16,
-                                    Over16_Female,
-                                    Over16_Female_Employed,
-                                    Over16_Employed,
-                                    Over16_ArmedForces,
-                                    Under5,
-                                    Age5to9,
-                                    Age10to14,
-                                    Age15to19,
-                                    Age20to24,
-                                    Age25to34,
-                                    Age35to44,
-                                    Age45to54,
-                                    Age55to59,
-                                    Age60to64,
-                                    Over65,
-                                    Under18,
-                                    Over18,
-                                    Over18_Male,
-                                    Over18_Female,
-                                    Over15_Male,
-                                    Over15_Female,
-                                    Over15_Male_Married,
-                                    Over15_Female_Married,
-                                    Nursery.preschool.enrollment,
-                                    Kindergarten.enrollment,
-                                    Householder_Children6to17,
-                                    Total.Population,
-                                    Total.Population.Male,
-                                    Total.Population.Female,
-                                    White,
-                                    Asian,
-                                    Native.American,
-                                    BusinessOccupation,
-                                    SalesOccupation,
-                                    NaturalResourcesOccupation,
-                                    AgricultureIndustry,
-                                    MfgIndustry,
-                                    WholesaleIndustry,
-                                    WarehousingIndustry,
-                                    InformationIndustry, 
-                                    InsuranceIndustry, 
-                                    OtherServicesIndustry,
-                                    EntertainmentIndustry,
-                                    Household.Income_Less10k, Household.Income_10to15k, 
-                                    Household.Income_15to25k, Household.Income_25to35k,
-                                    Household.Income_35to50k ))
-
-colval.names <- colnames(acs.df.subset)
-
-
-for (c in colval.names[-c(1)]) {
-  create_market_basket(c)
 }
 
 
-# txn <- read.transactions("data/acs_market_basket.csv")
-# inspect(txn)
+# Create Market basket file for NY other
+census.filename <- "data/Census_Ny_Other.csv"
+market_basket.filename <- "data/market_basket_ny_other.csv"
+market_basket_wrapper(census.filename, market_basket.filename)
+
+# Create Market basket file for NYC
+# census.filename.nyc <- "data/Census_Nyc.csv"
+# market_basket.nyc.filename <- "data/market_basket_nyc.csv"
+# market_basket_wrapper(census.filename.nyc, market_basket.nyc.filename)
+
 
 
